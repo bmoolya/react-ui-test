dedb7374ac72fde99d9c15efb3258f86fa08df03
bmoolya <33536546+bmoolya@users.noreply.github.com>
2017-11-12 19:58:11 +0530

    Update devices.js

--- src/js/components/devices.js
+++ src/js/components/devices.js
@@ -21,7 +21,7 @@ class Devices extends Component{
     }
     return (
       <ListGroup style={listStyle}>
-        <ListGroupItem style={{borderRadius: 0, height: '10%'}}>
+        <ListGroupItem style={{borderRadius: 0, height: '8%'}}>
         <div style={{verticalAlign: 'middle'}}>
           <div style={{float:'left'}}><h4>Devices</h4></div>
           <div style={{float:'right'}}>
@@ -32,21 +32,15 @@ class Devices extends Component{
           </div>
         </div>
         </ListGroupItem>
-        <ListGroupItem style={{borderRadius: 0, height: '9%', padding: 0, margin: 0}}>
-        <Navbar style={{backgroundColor: 'white', borderRadius: 0, boxShadow: 'none',
-        padding: 0, margin: 0,border: 'none'}}>
-    <Navbar.Form>
-      <FormGroup >
-        <InputGroup>
-          <InputGroup.Addon><span className="glyphicon glyphicon-search"></span></InputGroup.Addon>
-          <FormControl type="text" placeholder="Search" />
-        </InputGroup>
-        </FormGroup>
-
-    </Navbar.Form>
-    </Navbar>
+        <ListGroupItem style={{borderRadius: 0, height: '10%', margin: 0, borderBottom: 0}}>
+          <div style={{float:'left', width: 250}}>
+            <InputGroup>
+              <InputGroup.Addon><span className="glyphicon glyphicon-search"></span></InputGroup.Addon>
+              <FormControl type="text" placeholder="Search" />
+            </InputGroup>
+          </div>
         </ListGroupItem>
-        <ListGroupItem style={{borderRadius: 0, height: '68%'}}>
+        <ListGroupItem style={{borderRadius: 0, height: '76%'}}>
           <Device />
           <Device />
         </ListGroupItem>
