dafcc5ee3272d70af114bcbc5983c31fe1fc1df7
bmoolya <33536546+bmoolya@users.noreply.github.com>
2017-11-12 19:59:44 +0530

    Update recipes.js

--- src/js/components/recipes.js
+++ src/js/components/recipes.js
@@ -27,7 +27,7 @@ class Recipes extends Component{
       </div>)
     return (
       <ListGroup style={listStyle}>
-        <ListGroupItem style={{borderRadius: 0, height: '10%'}}>
+        <ListGroupItem style={{borderRadius: 0, height: '8%'}}>
         <div style={{verticalAlign: 'middle'}}>
           <div style={{float:'left'}}><h4>Recipes</h4></div>
           <div style={{float:'right'}}>
@@ -38,20 +38,22 @@ class Recipes extends Component{
           </div>
         </div>
         </ListGroupItem>
-        <ListGroupItem style={{borderRadius: 0, height: '9%', padding: 0, margin: 0}}>
-        <Navbar style={{backgroundColor: 'white', borderRadius: 0, boxShadow: 'none',
-            padding: 0, margin: 0,border: 'none'}}>
-          <Navbar.Form>
-            <FormGroup >
-              <InputGroup>
-                <InputGroup.Addon><span className="glyphicon glyphicon-search"></span></InputGroup.Addon>
-                <FormControl type="text" placeholder="Search" />
-              </InputGroup>
-              </FormGroup>
-          </Navbar.Form>
-        </Navbar>
+        <ListGroupItem style={{borderRadius: 0, height: '10%', margin: 0, borderBottom: 0}}>
+          <div style={{float:'left', width: 250}}>
+            <InputGroup>
+              <InputGroup.Addon><span className="glyphicon glyphicon-search"></span></InputGroup.Addon>
+              <FormControl type="text" placeholder="Search" />
+            </InputGroup>
+          </div>
+          <div style={{float:'right'}}>
+          <DropdownButton bsSize="small" title="All Systems" id="dropdown-size-extra-small">
+            <MenuItem eventKey="1">XM04</MenuItem>
+            <MenuItem eventKey="2">XM05</MenuItem>
+            <MenuItem eventKey="3">XM06</MenuItem>
+          </DropdownButton>
+          </div>
         </ListGroupItem>
-        <ListGroupItem style={{borderRadius: 0, height: '68%'}}>
+        <ListGroupItem style={{borderRadius: 0, height: '76%'}}>
         <ListGroup style={listItemStyle}>
              <ListGroupItem header="MCK Seperation - Acme" style={listItemStyle}>
              <div style={{verticalAlign: 'middle'}}>
